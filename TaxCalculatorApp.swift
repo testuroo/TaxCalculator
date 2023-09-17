bd8f7750212425995946f409565fe7eb011af077
testuroo <145014365+testuroo@users.noreply.github.com>
2023-09-18 02:01:27 +0900

    Add files via upload

---
+++ TaxCalculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Tetsurou Morishita on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+            
+        }
+    }
+}
